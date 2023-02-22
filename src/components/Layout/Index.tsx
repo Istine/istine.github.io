14d1fb6021f16950debc3e60e4e08acce953d8c6
istine <imanchesebastian@gmail.com>
2023-02-22 14:59:18 +0100

    added max width prop for xl screens

--- src/components/Layout/Index.tsx
+++ src/components/Layout/Index.tsx
@@ -5,7 +5,15 @@ import PageMenuBar from "./PageMenuBar";
 
 const Layout: React.FC<{ children: any }> = ({ children }) => {
   return (
-    <Box sx={{ p: 0, overflow: "hidden", width: "100%" }}>
+    <Box
+      sx={{
+        p: 0,
+        overflow: "hidden",
+        width: "100%",
+        maxWidth: "1300px",
+        margin: "0 auto",
+      }}
+    >
       <Ad />
       <UserMenuBar />
       <PageMenuBar />
